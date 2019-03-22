ff8bfe6f056592bd001557121f91074da270cd85
Rémi Taniel <taniel.remi@gmail.com>
2019-03-22 15:35:50 +0100

    fix(PPO): fix tp2 tests

--- PPO/tp2/operations_historisees/Compte.java
+++ PPO/tp2/operations_historisees/Compte.java
@@ -1,18 +1,15 @@
-import java.util.*;
-import java.lang.*;
-
 class Compte {
 
     public static int MAX_OPERATIONS = 10;
-    
+
     public int dernierCredit, dernierDebit;
     public double[] credits, debits;
 
     public Compte() {
         this.credits = new double[MAX_OPERATIONS];
         this.debits = new double[MAX_OPERATIONS];
-        this.dernierCredit = -1;
-        this.dernierDebit = -1;
+        this.dernierCredit = 0;
+        this.dernierDebit = 0;
     }
 
     public Compte(double credit) {
@@ -23,7 +20,7 @@ class Compte {
     public double getCredit() {
         double sum = 0.;
 
-        for (int i = 0; i <= this.dernierCredit; i++)
+        for (int i = 0; i < this.dernierCredit; i++)
             sum += this.credits[i];
 
         return sum;
@@ -32,30 +29,34 @@ class Compte {
     public double getDebit() {
         double sum = 0.;
 
-        for (int i = 0; i <= this.dernierDebit; i++)
+        for (int i = 0; i < this.dernierDebit; i++)
             sum += this.debits[i];
 
         return sum;
     }
 
     public void crediter(double x) {
-        if (this.dernierCredit > MAX_OPERATIONS - 1) {
-            this.credits[0] = this.getCredit();
-            this.dernierCredit = 0;
-        }
-
-        this.dernierCredit++;
         this.credits[this.dernierCredit] = x;
+        this.dernierCredit++;
+
+        if (this.dernierCredit == MAX_OPERATIONS - 1) {
+            double sum = this.getCredit();
+            this.credits = new double[MAX_OPERATIONS];
+            this.credits[0] = sum;
+            this.dernierCredit = 1;
+        }
     }
 
     public void debiter(double x) {
-        if (this.dernierDebit > MAX_OPERATIONS - 1) {
-            this.debits[0] = this.getCredit();
-            this.dernierDebit = 0;
-        }
-
-        this.dernierDebit++;
         this.debits[this.dernierDebit] = x;
+        this.dernierDebit++;
+
+        if (this.dernierDebit == MAX_OPERATIONS - 1) {
+            double sum = this.getDebit();
+            this.debits = new double[MAX_OPERATIONS];
+            this.debits[0] = sum;
+            this.dernierDebit = 1;
+        }
     }
 
     public double solde() {
