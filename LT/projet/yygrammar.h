92ad443ec1ed141e64490b2cd835ebc60761f2c1
Remi <taniel.remi@gmal.com>
2019-06-07 15:04:17 +0200

    feat(LT): begin project

---
+++ LT/projet/yygrammar.h
@@ -0,0 +1,22 @@
+#ifndef YYSTYPE
+#define YYSTYPE long
+#endif
+extern YYSTYPE yylval;
+extern long yypos;
+
+#define BEGIN 256
+#define TITLE 257
+#define AUTHOR 258
+#define DATE 259
+#define MAKETITLE 260
+#define SECTION 261
+#define SUBSECTION 262
+#define SUBSUBSECTION 263
+#define TEXTBF 264
+#define TEXTIT 265
+#define ITEMIZE 266
+#define ENUMERATE 267
+#define ITEM 268
+#define BACKSLASH 269
+#define LDOTS 270
+#define WORD 271
